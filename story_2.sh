3974568f827cc0422abb5b2a989948470c4fb485
girirajmpatil <101155931+girirajmpatil@users.noreply.github.com>
2022-09-02 20:02:50 +0530

    Update story_2.sh

--- story_2.sh
+++ story_2.sh
@@ -2,15 +2,28 @@
 
 echo "Installing AWS Cli"
 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 
 aws --version  # aws-cli/2.7.9 Python/3.9.11 Windows/10 exe/AMD64 prompt/off
 
-p="/home/joy-buresh_7/.aws/credentials"
+#p="/home/joy-buresh_7/.aws/credentials"
+
+
+#p="~/.aws/credentials"
+
+p="C:\Users\USERNAME\.aws\credentials"
+
+find -f [.aws\credentials]
+
+cat path >> newcredfile
+
+docker image <git_hub_path> 
+
+
 
 #touch ~/.aws/credentials
 
-if [ -e p]
+if [ -e p ]
 then
         {
 
@@ -27,12 +40,14 @@ region = ap-south-1
 output = json" > $p
 
 }
+
 fi
-aws ec2 run-instances \
-        --image-id ami-06489866022e12a14 \
-        --instance-type t2.small \
-        --subnet-id subnet-0e8281328042b2fa4 \
-        --key-name Joykey123 \
-        --security-group-ids sg-09bacda5b0f9f1910 \
-        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=JoyAWSCli}]' \
-        --region ap-south-1
+
+#aws ec2 run-instances \
+       --image-id ami-06489866022e12a14 \
+       --instance-type t2.small \
+       --subnet-id subnet-0e8281328042b2fa4 \
+       --key-name Joykey123 \
+       --security-group-ids sg-09bacda5b0f9f1910 \
+       --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=JoyAWSCli}]' \
+       --region ap-south-1
