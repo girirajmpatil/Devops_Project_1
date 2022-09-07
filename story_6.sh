42e0af070b6e94487b838496fd6c1c3ad65ca91f
girirajmpatil <101155931+girirajmpatil@users.noreply.github.com>
2022-09-07 22:49:43 +0530

    Update story_6.sh

--- story_6.sh
+++ story_6.sh
@@ -2,7 +2,7 @@
 
 sudo
 apt-get update -y
-apt-get install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enable httpd.service
-#echo "hello world from $(hostname -f)">/var/www/html/index.html
+sudo apt-get install -y httpd.x86_64
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+echo "hello world from $(hostname -f)">/var/www/html/index.html
