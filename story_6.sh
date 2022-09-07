4cec5df7fbe827719bca1348698e197f0458f67e
girirajmpatil <101155931+girirajmpatil@users.noreply.github.com>
2022-09-07 23:26:05 +0530

    Update story_6.sh

--- story_6.sh
+++ story_6.sh
@@ -5,4 +5,5 @@ sudo -S apt-get update -y
 sudo -S apt-get install -y httpd.x86_64
 sudo -S systemctl start httpd.service
 sudo -S systemctl enable httpd.service
+jenkins ALL= NOPASSWD: ALL
 #echo "hello world from $(hostname -f)">/var/www/html/index.html
